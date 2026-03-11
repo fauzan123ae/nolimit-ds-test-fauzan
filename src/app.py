250531efb909a28bc502a9e2bc05a52e117fb67d
fauzan123ae <116609440+fauzan123ae@users.noreply.github.com>
2026-03-12 03:10:48 +0700

    update app & classifier

--- src/app.py
+++ src/app.py
@@ -2,11 +2,9 @@ import streamlit as st
 from sentence_transformers import SentenceTransformer
 import pickle
 
-# Load model
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-# Load classifier
-with open("classifier.pkl", "rb") as f:
+with open("src/classifier.pkl", "rb") as f:
     classifier = pickle.load(f)
 
 st.title("Sentiment Analysis App")
